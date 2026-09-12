47042d568b3cd237b6e04bc60c0551d4db6a75e8
albilu <35330562+albilu@users.noreply.github.com>
2026-09-12 10:55:03 +0200

    feat(gtk): share size formatting in import-sequence disk label

--- odm-gtk4/src/main/java/org/odm/gtk4/ImportSequenceDialog.java
+++ odm-gtk4/src/main/java/org/odm/gtk4/ImportSequenceDialog.java
@@ -402,7 +402,7 @@ public class ImportSequenceDialog {
     private void updateDiskSpace(String dir) {
         try {
             long free = new java.io.File(dir).getUsableSpace();
-            diskSpaceLabel.setLabel(String.format("%.2f GB free", free / (1024.0 * 1024 * 1024)));
+            diskSpaceLabel.setLabel(DownloadFormats.size(free) + " free");
         } catch (Exception e) {
             diskSpaceLabel.setLabel("");
         }
